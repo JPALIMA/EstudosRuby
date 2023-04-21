5cb63132f1aa974a70820dba81a9c9155b6455fb
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-04-20 23:04:45 -0300

    algoritmo pesquisa linear

---
+++ .vscode/LinearSearch.rb
@@ -0,0 +1,16 @@
+## LinearSearch Algoritmo
+    pesquisa de classe
+    attr_reader :matriz, :número
+
+    def def initialize(matriz, número)
+      @array = matriz
+      @number = número
+    end
+    def LinearSearch
+        matriz. each_with_index fezer | elemento, índice|
+            return "Element #{element} is present at index #{index}" se @number == element
+    end
+    "O elemento #{@number} não está presente na matriz"
+    end
+end
+fimded
