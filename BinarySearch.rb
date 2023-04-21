22873ad7c121e1ef71769a179ab2db950c607b82
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-04-20 23:56:52 -0300

    codigo da pesquisa  binário

---
+++ BinarySearch.rb
@@ -0,0 +1,19 @@
+def binary_search(n, arr)
+    middle = arr.length / 2
+    i = 0
+    j = arr.length - 1
+
+    while i < j
+        if arr[middle] == n
+            return true
+        elsif arr[middle] < n
+            i = middle + 1
+            middle + 1
+            middle = i + J / 2
+        else
+            j = middle - 1
+            middle = 1 + j / 2
+        end
+    end
+    false
+end
