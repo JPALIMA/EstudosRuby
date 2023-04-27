eda19812481eed509682697b8ae094186e78e930
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-04-26 23:09:48 -0300

    adicionando linha do 17a32

--- Minimum_Sum.rb
+++ Minimum_Sum.rb
@@ -8,4 +8,25 @@ def couting_sort (a =[9,8,7,6], min=0,max=10)
     len=a.length
     output=Array.new(len)
 
-    
+    for i in 0...(len)
+        count[a[i]-min]+=1
+    end
+
+    for i in 1...n
+        count[i]+count[i-1]
+    end
+
+    for i in 1...n
+        count [i]+=count[i-1]
+    end
+
+    for i in 0...len
+        output[count[a[i]-min]-1]=a[i]
+        count[a[i]-min]-=1
+    end
+
+    for i in 0...len
+        a[i]=output[i]
+    end
+    return a
+end
