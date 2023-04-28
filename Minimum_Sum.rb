1dd63ae0f4b6f4bb060b9d56cec2e34d249912d5
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-04-28 00:06:11 -0300

    alteração de código

--- Minimum_Sum.rb
+++ Minimum_Sum.rb
@@ -1,17 +1,15 @@
 def minimum_sum(arr, k)
     arr.sort!
 
-min_sum = 0
-(0..k-1).each do |i|
-min_sum += arr[i]
-end 
-
-return min_sum
+    min_sum = 0
+    min_sum += arr[i]
 end
 
-arr = [3, 5, 2, 7, 9, 8]
-k = 3
+return minimum_sum 
+end
 
+arr = [3, 5, 3, 7, 9, 8]
+k = 3
 puts "Array: #{arr}"
-puts "k: #{arr}"
-puts "Minimum Sum: #{minim_sum(arr, k)}"
+puts "k: #{k}"
+puts "Minimum Sum: #{minimum_sum(arr, k)}"
