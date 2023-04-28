be62555ac0667d274bb5eeb093d7d78fc5433fca
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-04-27 22:54:24 -0300

    Adicionando linha do 1a6

--- Minimum_Sum.rb
+++ Minimum_Sum.rb
@@ -1,32 +1,5 @@
-def couting_sort (a =[9,8,7,6], min=0,max=10)
-    if min>max
-        return "invalid range"
-    end
+def minimum_sum(arr, k)
+    arr.sort!
+    
+    
 
-    n=max-min+1
-    count=array.new(n,0)
-    len=a.length
-    output=Array.new(len)
-
-    for i in 0...(len)
-        count[a[i]-min]+=1
-    end
-
-    for i in 1...n
-        count[i]+count[i-1]
-    end
-
-    for i in 1...n
-        count [i]+=count[i-1]
-    end
-
-    for i in 0...len
-        output[count[a[i]-min]-1]=a[i]
-        count[a[i]-min]-=1
-    end
-
-    for i in 0...len
-        a[i]=output[i]
-    end
-    return a
-end
