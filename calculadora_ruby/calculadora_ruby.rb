755a52a624e8ba4ee11aab6a440d134acd797714
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-01 14:08:55 -0300

    adicionado linha na calculadora

---
+++ calculadora_ruby/calculadora_ruby.rb
@@ -0,0 +1,41 @@
+class Calculadora
+    def sumar (n,m)
+        n+m
+    end
+
+    def restar (m,n)
+        n-m
+    end
+
+    def multiplicar(n,m)
+        n*m
+    end
+
+    def
+        dividir(n,m)
+        n/m
+    end
+end
+
+print "Primer valor:"
+val1 = gets.chomp.to_i
+
+print "Segundo valor:"
+val2 = gets.chomp.to_i
+  
+print "Operador (+,-,*,/):"
+operador = gets.chomp
+
+case operador
+when "+"
+    puts Calculadora.new.sumar(val1,val2)
+when "-"
+    puts Calculadora.new.restar(val1, val2)
+when "*"
+    puts Calculadora.new.multiplicar(val1,val2)
+when "/"
+    puts Calculadora.new.dividir(val1,val2)
+else
+    puts "Operador inválido"
+end 
+
