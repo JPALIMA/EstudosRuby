340df8c30325de0e622f04fbfd0ecd145ba21411
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-06 22:42:06 -0300

    adicionando classe_carro2 e renomeando calculadora_1

---
+++ OO_RUBY/classe_carro2.rb
@@ -0,0 +1,22 @@
+class Car 
+    attr_accessor :make, :model, :year, :color
+
+    def initialize(make, model, year, color)
+        @make = make
+        @model = model
+        @year = year
+        @color = color
+    end
+
+    def start
+        puts "Starting the #{@make} #{@model}..."
+    end
+
+    def stop 
+        puts "Stopping the #{make} #{@model}..."
+    end
+end
+
+my_car = Car.new("Toyota", "Corolla", 2021, "blue")
+my_car.start
+my_car.stop
