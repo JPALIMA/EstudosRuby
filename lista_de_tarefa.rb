0044849466675ac2b3443249590cb8861928ee22
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-06 22:24:59 -0300

    modificando arquivos:bubbleSort.rb, calculadora_ruby,lista_de_tarefa.rb

---
+++ lista_de_tarefa.rb
@@ -0,0 +1,16 @@
+tasks = []
+
+loop do
+    puts "Digite uma tarefa para adicionar à lista (ou 'exit' para sair): "
+    input = gets.chomp
+
+    break if input == 'exit'
+
+    tasks << input
+    puts "Tarefa adicionada à lista. "
+end
+
+puts "Lista de tarefas: "
+tasks.each_with_index do |task, index|
+    puts "#{index+1}. #{task}"
+end
