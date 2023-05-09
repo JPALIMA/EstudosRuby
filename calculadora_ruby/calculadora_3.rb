075be9715fe09dc89e08988d0bb22c63c58ccea4
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-08 22:31:56 -0300

    modificado os arquivos com os codigos: calculadora_3.rb e jogo_de_adivinhacao.rb

--- calculadora_ruby/calculadora_3.rb
+++ calculadora_ruby/calculadora_3.rb
@@ -4,7 +4,7 @@ num1 = gets.chomp.to_f
 puts "Digite o segundo número: "
 num2 = gets.chomp.to_f
 
-puts "Qual operação deseja fazer (+ - * /)"
+puts "Qual operação deseja fazer? (+ - * /)"
 operacao = gets.chomp
 
 case operacao
@@ -18,7 +18,7 @@ when "/"
     resultado = num1 / num2
 else
     puts "Operação inválida"
-    exit 
+    exit
 end
 
 puts "O resultado é #{resultado}"
