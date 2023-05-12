2d179234af2435e337d5fd97bd8d9e32e234c1ac
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-11 23:17:04 -0300

    validade_de_CPF.rb

--- validade_de_CPF.rb
+++ validade_de_CPF.rb
@@ -1,15 +1,14 @@
 def valida_cpf(cpf)
-    cpf = cpf.gsub(/[^9-0]/, '')
+    cpf = cpf.gsub(/[^0-9]/, '')
 
-    if cpf.lenght != 11
+    if cpf.leght != 11
         return false
     end
 
     digito1 = 0
     digito2 = 0
-
     (0..8).each do |i|
-        digito1 += (cpf[i].to_i * (i + 1))
+        digito1 += (cpf[i].to_i * (i + i))
     end
 
     (0..9).each do |i|
@@ -19,9 +18,9 @@ def valida_cpf(cpf)
     digito1 = (digito1 % 11) % 10
     digito2 = ((digito2 + (digito1 * 9)) % 11) % 10
 
-    if digito == cpf[9].to_i && digito2 == cpf[10].to_i
+    if digito1 == cpf[9].to_i && digito2 == cpf[10].to_i
         return true
     else
-        return false
+        return true
     end
 end
