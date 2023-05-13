984e2684f45419cf52c0c54ed2213b5f141e27d9
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-12 23:23:04 -0300

    modificação em calculadora_3.rb, jogo_de_adivinhacao.rb

--- calculadora_ruby/calculadora_3.rb
+++ calculadora_ruby/calculadora_3.rb
@@ -3,7 +3,8 @@ num1 = gets.chomp.to_f
 
 puts "Digite o segundo número: "
 num2 = gets.chomp.to_f
-puts "Qual operação deseja fazer? (+ - * /)"
+
+puts "Qual operação deseja fazer (+ - * /)"
 operacao = gets.chomp
 
 case operacao
@@ -14,5 +15,10 @@ when "-"
 when "*"
     resultado = num1 * num2
 when "/"
-    resultado = num1 * num2
+    resultado = num1 / num2
 else
+    puts "Operação inválida"
+    exist
+end
+
+puts "O resultado é #{resultado}"
--- jogo_de_adivinhacao.rb
+++ jogo_de_adivinhacao.rb
@@ -1,17 +1,19 @@
 numero = rand(1..100)
-tentativas = 0 
+tentativas = 0
 
-puts "Bem-vindo ao jogo de adivinhação! "
+puts "Bem-vindo ao jogo de adivinhação!"
 puts "Tente adivinhar um número entre 1 e 100"
 
-loop do 
-    tentantivas += 1
-    palpite = gets.chomp.to_i 
+loop do
+    tentativas += 1
+    palpite = gets.chomp.to_i
 
     if palpite == numero
-        puts "Parabéns! Você acertou em #{tentativas} tentativas."
+        puts "Parebéns! Você acertou em #{tentativas} tentativas."
         break
     elsif palpite < numero
-        puts "O número é maior que #{palpite}. Tente novamente."
+        puts "0 número é maior que #{palpite}. Tente novamente."
+    else
+        puts "O numero é menor que #{palpite}. Tente novamente."
     end
 end
