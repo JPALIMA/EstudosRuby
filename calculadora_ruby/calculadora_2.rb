057ded7922bd51c7d88d92d62329b4f182c00433
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-15 20:33:33 -0300

    BubbleSort.RB, calculadora_2.rb, lista_de_tarefa.rb, validade_de_CPF.rb

--- calculadora_ruby/calculadora_2.rb
+++ calculadora_ruby/calculadora_2.rb
@@ -1,10 +1,10 @@
 puts "Digite o primeiro número: "
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_i 
 
 puts "Digite o segundo número: "
 num2 = gets.chomp.to_i
 
 puts "A soma é #{num1 + num2}"
-puts "A subtração é #{num1 - num2}"
+puts "A substração é #{num1 - num2}"
 puts "A multiplicação é #{num1 * num2}"
 puts "A divisão é #{num1 / num2}"
