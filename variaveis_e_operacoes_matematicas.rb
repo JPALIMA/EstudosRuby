bfa5b9b8b8ac132c7a3abc4a7e925621e3a0db1a
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-18 21:47:07 -0300

    adicionando variaveis_e_operaçoes_matematicas.rb

---
+++ variaveis_e_operacoes_matematicas.rb
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b 
+divisao = a / b
+
+puts "Soma: #{soma}"
+puts "Subtracão: #{subtracao}"
+puts "Multiplicação: #{multiplicacao}"
+puts "Divisão: #{divisao}"
