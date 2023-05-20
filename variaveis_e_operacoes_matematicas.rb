0200c99e56a7ee40b3036184c6ed6bb57c0d0a14
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-19 23:08:45 -0300

    modificando codigo: vatiaveis_e_operacoes_matematicas.rb

--- variaveis_e_operacoes_matematicas.rb
+++ variaveis_e_operacoes_matematicas.rb
@@ -2,10 +2,10 @@ a = 5
 b = 10
 soma = a + b
 subtracao = a - b
-multiplicacao = a * b 
+multiplicacao = a * b
 divisao = a / b
 
 puts "Soma: #{soma}"
-puts "Subtracão: #{subtracao}"
+puts "Subtração: #{subtracao}"
 puts "Multiplicação: #{multiplicacao}"
 puts "Divisão: #{divisao}"
