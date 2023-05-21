702fd0c64989d456a3ad588f9e288c6de7eca4e2
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-20 23:42:47 -0300

    adicionando codigo: calculadora_4.rb

---
+++ calculadora_ruby/calculadora_4.rb
@@ -0,0 +1,26 @@
+    def dividir(n,m)
+        n/m
+    end
+end
+
+print "Primeiro valor:"
+val1 = gets.chomp.to_i
+
+print "Segundo valor:"
+val2 = gets.chomp.to_i
+
+print "Operador (+,-,*,/):"
+operador = gets.chomp
+
+case operador 
+when "+"
+    puts Calculadora.new.sumar(val1,val2)
+when "-"
+    puts Calculadora.new.restar(val1,val2)
+when "*"
+    puts Calculadora.new.multiplicar(val1,val2)
+when "/"
+    puts Calculadora.new.dividir(val1,val2)
+else
+    puts "Operador inválido"
+end
