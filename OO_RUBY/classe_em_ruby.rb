b57eb619a9799ada484a4d0eec754694ec316e91
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-24 23:02:12 -0300

    modificando codigos: classe_em_ruby.rb, estrutura_condicional.rb

--- OO_RUBY/classe_em_ruby.rb
+++ OO_RUBY/classe_em_ruby.rb
@@ -1,15 +1,14 @@
-class pessoa
-    attr_accessor :nome, :idade
-
+class Pessoa attr_accessor :nome, :idade
+    
     def initialize(nome, idade)
         @nome = nome
         @idade = idade
     end
 
     def saudacao
-        puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos."
+        puts "Olá, meu nome é #{@nome} e tenho #{@idade} anos. "
     end
 end
 
 pessoa = Pessoa.new("João", 25)
-pessoa.saudacao #Exemplo de uso: cria um objeto Pessoa e chama o método saudação
+pessoa.saudacao #Exemplo de uso: cria um ojetivo Pessoa e chama o método saudacao
--- estrutura_condicional.rb
+++ estrutura_condicional.rb
@@ -2,6 +2,6 @@ idade = 18
 
 if idade >= 18
     puts "Pode dirigir!"
-else
-    puts "Não Pode dirigir!"
+else 
+    puts "Não pode dirigir!"
 end
