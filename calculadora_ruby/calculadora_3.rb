ebe8f98ab41c66791ef4665d4bbab7ae0ee59a96
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-29 23:52:41 -0300

    modificando codigo: calculadora_3.rb

--- calculadora_ruby/calculadora_3.rb
+++ calculadora_ruby/calculadora_3.rb
@@ -1,24 +1,24 @@
-puts "Digite o primeiro número:"
+puts "Digite o primeiro número: "
 num1 = gets.chomp.to_f
 
-puts "Digite o segundo número:"
-num2 = gets.chomp.to_f
+puts "Digite o primeiro número:"
+num1 = gets.chomp.to_f
 
 puts "Qual operação deseja fazer? (+ - * /)"
 operacao = gets.chomp
 
-case operacao 
+case operacao
 when "+"
-    resultado = num1 + num2 
+    resultado = num1 + num2
 when "-"
     resultado = num1 - num2
 when "*"
     resultado = num1 * num2
 when "/"
-    resultado = num / num2
+    resultado = num1 / num2
 else
     puts "Operação inválida"
-    exit 
+    exit
 end
 
 puts "O resultado é #{resultado}"
