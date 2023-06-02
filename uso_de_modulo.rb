f0661cfb36b60f6a70cfb61c5372b326d44ed371
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-01 23:40:43 -0300

    modificando codigo: uso_de_modulo.rb

--- uso_de_modulo.rb
+++ uso_de_modulo.rb
@@ -1,15 +1,6 @@
 module Matematica
-    PI = 3.1416 
+    PI = 3.1416
 
-    def self.dobro(numero)
+    def self.dobro (numero)
         numero * 2
     end
-
-    def self.raiz_quadrada(numero)
-        Math.sqrt(numero)
-    end
-end
-
-puts Matematica::PI
-puts Matematica.dobro(5)
-puts Matematica.raiz_quadrada(16)
