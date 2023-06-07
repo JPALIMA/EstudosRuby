e7b6c429ce64f166004daacb49ab464b54aa7ba6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-06 21:56:16 -0300

    correcao no codigo vcalculadora_ruby/calculadora_4.rb

--- calculadora_ruby/calculadora_4.rb
+++ calculadora_ruby/calculadora_4.rb
@@ -1,26 +1,31 @@
-    def dividir(n,m)
-        n/m
+class Calculadora
+    def dividir(n, m)
+        n / m
     end
 end
 
-print "Primeiro valor:"
-val1 = gets.chom.to_i
+print "Primeiro valor: "
+val1 = gets.chomp.to_i
 
-print "Segundo valor:"
-val2 = gets.chomp_i
+print "Segundo valor: "
+val2 = gets.chomp.to_i
 
-print "Operador (+,-,*,/):"
+print "Operador (+, -, *, /): "
 operador = gets.chomp
 
 case operador
 when "+"
-    puts Calculadora.new.sumar(val1, val2)
+    puts val1 + val2
 when "-"
-    puts Calculadora.new.restar(val1, val2)
+    puts val1 - val2
 when "*"
-    puts Calculadora.new.multiplicar(val1, val2)
+    puts val1 * val2
 when "/"
-    puts Calculadora.new.dividir(val1, val2)
+    if val2 != 0
+        puts Calculadora.new.dividir(val1, val2)
+    else
+        puts "Divisão por zero não é permitida. "
+    end
 else
     puts "Operador inválido"
 end
