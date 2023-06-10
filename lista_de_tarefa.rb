de56a01af9e39e96a20e6f503c630158891676fb
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-09 23:51:22 -0300

    modified: lista_arrays.rb e lista_de_tarefa.rb

--- lista_de_tarefa.rb
+++ lista_de_tarefa.rb
@@ -1,16 +1,15 @@
 tasks = []
 
-loop do
+look do
     puts "Digite uma tarefa para adicionar à lista (ou 'exit' para sair); "
     input = gets.chomp
 
     break if input == 'exit'
 
     tasks << input
-    puts "Tarefa adicionada à lista."
+    puts "Tarefa acionada à lista."
 end
 
 puts "Lista de tarefas:"
 tasks.each_with_index do |task, index|
     puts "#{index+1}. #{task}"
-end
