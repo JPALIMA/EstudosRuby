c7340550b174d7454751436fe2d2ed267ece2d24
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-14 22:48:34 -0300

    alteracao/correcao  nas linhas do codigo: 12-18. O codigo apresentava o mesmo erro de numero menor, foi acrescentado um condicao em que numero do palpite for maior, apresentar, para digitar um numero maior. E acrescentando um break para finalizar o codigo em caso de loop

--- jogo_de_adivinhacao.rb
+++ jogo_de_adivinhacao.rb
@@ -1,16 +1,22 @@
 numero = rand(1..100)
-tentativas = 0 
+tentativas = 0
 
 puts "Bem-vindo ao jogo de adivinhação!"
 puts "Tente adivinhar um número entre 1 e 100"
 
 loop do 
-    tentativas += 1 
+    tentativas += 1
     palpite = gets.chomp.to_i
 
     if palpite == numero
         puts "Parabéns! Você acertou em #{tentativas} tentativas."
-    else
+        break
+    elsif palpite > numero
         puts "O número é menor que #{palpite}. Tente novamente."
+    elsif
+        palpite < numero
+        puts "O numero é maior que #{palpite}. Tente novamente."
     end
 end
+
+
