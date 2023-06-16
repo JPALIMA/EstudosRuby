be15a002de2eade2c6104894fc5485b22e0411b2
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-15 22:55:19 -0300

    modified: na linha do codigo nº 16, alteração para else ao invés de elsif

--- jogo_de_adivinhacao.rb
+++ jogo_de_adivinhacao.rb
@@ -1,10 +1,10 @@
 numero = rand(1..100)
 tentativas = 0
 
-puts "Bem-vindo ao jogo de adivinhação!"
+puts "Bem vindo ao jogo de adivinhação!"
 puts "Tente adivinhar um número entre 1 e 100"
 
-loop do 
+loop do
     tentativas += 1
     palpite = gets.chomp.to_i
 
@@ -13,10 +13,8 @@ loop do
         break
     elsif palpite > numero
         puts "O número é menor que #{palpite}. Tente novamente."
-    elsif
+    else
         palpite < numero
-        puts "O numero é maior que #{palpite}. Tente novamente."
+        puts "O número é maior que #{palpite}. Tente novamente."
     end
 end
-
-
