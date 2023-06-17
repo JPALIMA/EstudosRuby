3d99ae62be634a2e7d1f57885fecb4139f5a3fe6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-16 22:12:27 -0300

    criação de codigo: CRUD exemplo, você pode criar, listar, atualizar e excluir pessoas

---
+++ CRUD/CRUD_Pessoa.rb
@@ -0,0 +1,67 @@
+class Pessoa
+    attr_accessor :nome, :idade
+
+    def initialize(nome, idade)
+        @nome = nome
+        @idade = idade
+    end
+end
+
+class CrudPessoa
+    def initialize
+        @pessoas = []
+    end
+
+    def criar_pessoa(nome, idade)
+        pessoa = Pessoa.new(nome, idade)
+        @pessoas << pessoa
+        puts "Pessoa criada com sucesso!"
+    end
+
+    def listar_pessoas
+        if @pessoas.empty?
+            puts "Não há pessoas cadastradas."
+        else
+            @pessoas.each do |pessoa|
+                puts "Nome: #{pessoa.nome}, Idade: #{pessoa.idade}"
+            end
+        end
+    end
+
+    def atualizar_pessoa(nome, nova_idade)
+        pessoa = @pessoas.find { |p| p.nome == nome }
+        if pessoa.nil?
+            puts "Pessoa não encontrada."
+        else
+            pessoa.idade = nova_idade
+            puts "Pessoa atualizada com sucesso!"
+        end
+    end
+
+    def excluir_pessoa(nome)
+        pessoa = @pessoas.find { |p| p.nome == nome }
+        if pessoa.nil?
+            puts "Pessoa não encontrada. "
+        else
+            @pessoas.delete(pessoa)
+            puts "Pessoa excluída com sucesso!"
+        end
+    end
+end
+
+# Exemplo de uso
+crud = CrudPessoa.new
+
+crud.criar_pessoa("João", 25)
+crud.criar_pessoa("Maria", 30)
+
+crud.listar_pessoas
+# Saída atualizada:
+#Nome: João, Idade: 26
+#Nome: Maria, Idade: 30
+
+crud.exluir_pessoa("Maria")
+
+crud.listar_pessoas
+# Saída atualizada:
+# Nome: João, Idade: 26
