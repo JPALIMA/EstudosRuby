0c0cc8bad7a998b6932db6c655eb3118cd1e989f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-17 23:19:20 -0300

    erro de sintaxe do metodo 'excluir.pessoa(id). Atualizadno para 'excluir_pessoa(id)

--- CRUD/CRUD_Pessoa2.rb
+++ CRUD/CRUD_Pessoa2.rb
@@ -1,5 +1,5 @@
 class Pessoa
-    attr_acessor :id, :nome, :idade
+    attr_accessor :id, :nome, :idade
 
     def initialize(id, nome, idade)
         @id = id
@@ -26,7 +26,60 @@ class CrudPessoa
             puts "Não há pessoas cadastradas."
         else
             @pessoas.each do |pessoa|
-                puts "ID: #{pessoa.id}, Nome: #{pessoa.nome}, Idade: #{pessoa.idade}"
+                puts "ID: #{pessoa.id}, Nome: #{pessoa.nome}, Idade: #{pessoa}"
             end
         end
     end
+
+    def buscar_pessoa(id)
+        pessoa = @pessoas.find { |p| p.id == id }
+        if pessoa.nil?
+            puts "Pessoa não encontrada."
+        else
+            puts "ID: #{pessoa.id}, Nome #{pessoa.nome}, Idade: #{pessoa.id}"
+        end
+    end
+
+    def atualizar_pessoa(id, nome, idade)
+        pessoa = @pessoas.find { |p| p.id == id }
+        if pessoa.nil?
+            puts "Pessoa não encontrada. "
+        else
+            pessoa.nome = nome
+            pessoa.idade = idade
+            puts "Pessoa atualizada com sucesso!"
+        end
+    end
+
+    def excluir_pessoa(id)
+        pessoa = @pessoas.find { |p| p.id == id }
+        if pessoa.nil?
+        puts "Pessoa não encontrada. "
+        else
+            @pessoas.delete(pessoa)
+            puts "Pessoa excluída com sucesso!"
+        end
+    end
+end
+
+# Exemplo de uso
+crud = CrudPessoa.new
+
+crud.criar_pessoa("João", 25)
+crud.criar_pessoa("Maria", 30)
+
+crud.listar_pessoas
+# Saída:
+#ID: 0, Nome: João, Idade: 25
+#ID: 1, Nome: Maria, Idade: 30
+
+crud.atualizar_pessoa(0, "João Silva", 26)
+
+crud.listar_pessoas
+#Saída atualizada:
+#ID: 0, Nome: João Silva, Idade: 26
+#ID: 1, Nome: Maria, Idade: 30
+
+crud.buscar_pessoa(1)
+#Saída:
+#ID: 1, Nome: Maria, Idade: 30
