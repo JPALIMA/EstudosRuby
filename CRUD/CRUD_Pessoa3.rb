4cc33df76a3e3634f2c4b6a65dfeecc42f0dc5d8
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-19 22:25:08 -0300

    correção de metodo

--- CRUD/CRUD_Pessoa3.rb
+++ CRUD/CRUD_Pessoa3.rb
@@ -13,8 +13,8 @@ class Pessoa
 end
 
 class CrudPessoa
-    def initialize
-        @pessoas = []
+    def initialzize
+        @pessoa = []
     end
 
     def criar_pessoa(nome, idade)
@@ -23,35 +23,5 @@ class CrudPessoa
         @pessoas << pessoa
     end
 
-    def listar_pessoas
-        @pessoas.each do |pessoa|
-            puts pessoa
-        end
-    end
 
-    def atualizar_pessoa(id, novo_nome, nova_idade)
-        pessoa = @pessoas.find { |p| p.id == id }
-        pessoa.nome = novo_nome
-        pessoa.idade = nova_idade
-    end
-
-    def buscar_pessoa(id)
-        pessoa = @pessoas.find { |p| p.id == id }
-        puts pessoa
-    end
-end
-
-# Exemplo de uso
-crud = CrudPessoa.new 
-
-crud.criar_pessoa("João", 25)
-crud.criar_pessoa("Maria", 30)
-
-crud.listar_pessoas
-#Saída:
-#ID: 0, Nome: João, Idade: 25
-#ID: 1, Nome: Maria, Idade: 30
-
-crud.buscar_pessoa(1)
-#Saída:
-#ID: 1, Nome: Maria, Idade: 30
+    
