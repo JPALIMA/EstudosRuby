7643925c325086affff5fcfebe0fd062b9850e58
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-20 21:26:42 -0300

    criação de um projeto simples de um blog, aplicando classe Post

---
+++ blogbasico_post.rb
@@ -0,0 +1,42 @@
+class Post
+    attr_accessor :title, :author, :content
+
+    def initialize(title, author, content)
+        @title = title
+        @author = author
+        @content = content
+    end
+end
+
+class Blog
+    attr_accessor :posts
+
+    def initialize 
+        @posts = []
+    end
+
+    def add_post(post)
+        @posts << post
+    end
+
+    def display_posts
+        @posts.each do |post|
+            puts "Title: #{post.title}"
+            puts "Author: #{post.author}"
+            puts "Content: #{post.content}"
+            puts "------------------------"
+        end
+    end
+end
+
+#Criando alguns posts
+post1 = Post.new("Olá, Mundo!", "Joao Pedro", "Bem vindo ao meu Blog")
+post2 = Post.new("Ruby é incrível!", "Joao Pedro", "Aqui estão algumas razões pelas quais Ruby é legal")
+
+# Criando um blog e adicionando os posts
+blog = Blog.new
+blog.add_post(post1)
+blog.add_post(post2)
+
+#Exibindo os posts do blog
+blog.display_posts
