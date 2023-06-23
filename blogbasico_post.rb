a8811b4567ece4f30ece3bcdff6a1c9e74970b99
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-22 23:52:03 -0300

    modified: blogbasico_post.rb

--- blogbasico_post.rb
+++ blogbasico_post.rb
@@ -18,3 +18,27 @@ class Blog
     def add_post(post)
         @posts << post
     end
+
+    def display_posts
+        @posts.each do |post|
+            puts "Title: #{post.title}"
+            puts "Author: #{post.author}"
+            puts "Content: #{post.content}"
+            puts "-------------------"
+        end
+    end
+
+    def cout_posts        
+        puts "Total posts: #{@posts.length}"
+    end
+    
+    def search_posts_by_author(author)
+        found_posts = @posts.select { |post| post.author == author}
+
+        if found_posts.empty?
+            puts "No posts found for author: #{author}"
+        else
+            puts "Posts by author #{author}:"
+            found_posts.each do |post|
+                puts "Title: #{post.title}"
+                
