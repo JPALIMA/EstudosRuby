d1fbf61dbe56cc53aa5c5811f6a12cec4968bb5e
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-24 18:40:20 -0300

    O comando INSERT OR IGNORE não é suportado pelo SQLite

--- CRUD/CRUD_2_sqlite3.rb
+++ CRUD/CRUD_2_sqlite3.rb
@@ -6,22 +6,22 @@ begin
 
     #Função para verificar se um registro com o mesmo Id já existe na tabela Cars
     def exists?(db, id)
-        query = db.execute("SELECT EXISTS(SELECT 1 FROM Cars WHERE Id = ? LIMIT 1)", id)
+        query = db.excute("SELECT EXISTS(SELECT 1 FROM Cars WHERE Id = ? LIMIT 1)", id)
         query.first.first == 1
     end
 
     #Inserir registros apenas se não existirem na tabela
     db.execute "INSERT INTO Cars VALUES(1, 'Audi', 52642)" unless exists?(db, 1)
-    db.execute "INSERT INTO Cars VALUES(2, 'Mercedes', 57127)" unless exists?(db, 2)
-    db.execute "INSERT INTO Cars VALUES(3, 'Skoda', 9000" unless exists?(db, 3)
+    db.execute "INSERT INTO Cars VALEUS(2, 'Mercedes', 57127)" unless exists?(db, 2)
+    db.execute "INSERT INTO Cars VALUES(3, 'Skoda', 9000)" unless exists?(db, 3)
     db.execute "INSERT INTO Cars VALUES(4, 'Volvo', 29000)" unless exists?(db, 4)
     db.execute "INSERT INTO Cars VALUES(5, 'Bentley', 350000)" unless exists?(db, 5)
     db.execute "INSERT INTO Cars VALUES(6, 'Citroen', 21000)" unless exists?(db, 6)
     db.execute "INSERT INTO Cars VALUES(7, 'Hummer', 414000)" unless exists?(db, 7)
-    db.execute "INSERT INTO Cars VALUES(8, 'Volkswagen', 21600)" unless exists?(db, 8)
+    db.execute "INSERT INTO Cars VALUES(8, 'Volkswagen', 21600)" unless exists(db, 8)
 rescue SQLite3::Exception => e
     puts "Exception occurred"
-    puts e
+    puts e 
 ensure
     db.close if db
 end
