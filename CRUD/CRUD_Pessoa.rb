ff52060bcd6f002343a7eaad88ab9aba7c7aff33
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-26 22:07:01 -0300

    correcao na endentação do codigo

--- CRUD/CRUD_Pessoa.rb
+++ CRUD/CRUD_Pessoa.rb
@@ -10,13 +10,12 @@ end
 class CrudPessoa
     def initialize
         @pessoas = []
-
     end
 
-    def criar_pessoa(nome, idade)
+    def criar_pessoa (nome, idade)
         pessoa = Pessoa.new(nome, idade)
         @pessoas << pessoa
-        puts "Pessoa criadad com sucesso!"
+        puts "Pessoa criada com sucesso!"
     end
 
     def listar_pessoas
@@ -30,34 +29,41 @@ class CrudPessoa
     end
 
     def atualizar_pessoa(nome, nova_idade)
-        pessoa = @pessoas.find { |p| p.nome == nome }
+        pesssoa = @pessoas.find { |p| p.nome == nome }
         if pessoa.nil?
             puts "Pessoa não encontrada. "
         else
-            @pessoa.idade = nova_idade
-            puts "Pessoa atualizada com sucesso!"
+            pessoa.idade = nova_idade
+            puts "Pessoa atualizda com sucesso!"
         end
     end
 
     def excluir_pessoa(nome)
         pessoa = @pessoas.find { |p| p.nome == nome }
-        if pessoa.nil?
-            puts "Pessoa excluída com sucesso! "
+            if pessoa.nil?
+                puts "Pessoa excluída com sucesso!"
+            end
         end
     end
-end
 
-#exemplo de uso
-crud = CrudPessoa.new
+    #exemplo de uso
+    crud = CrudPessoa.new
+    
+    crud.criar_pessoa("João", 25)
+    crud.criar_pessoa("Maria", 30)
 
-crud.criar_pessoa("João", 25)
-crud.criar_pessoa("Maria", 30)
+    crud.listar_pessoas
+    #Saída:
+    #Nome: João, idade: 25
+    #Nome: Maria, idade: 30
 
-crud.listar_pessoas
-#Saída atualizada:
-#Nome: João, idade: 26
-#Nome: Maria, Idade: 30
+    crud.atualizar_pessoa("João", 26)
 
-crud.listar_pessoas
-#Saída atualizada:
-#Nome: João, Idade: 26
+    crud.listar_pessoas
+    #Saída atualizada:
+    #Nome: João, idade: 26
+    #Nome: Maria, Idade: 30
+
+    crud.listar_pessoas
+    #Saída atualizada:
+    #Nome: João, Idade: 26
