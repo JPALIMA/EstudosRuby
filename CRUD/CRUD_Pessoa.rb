913677e941ada35d5a1f3e0b3adb4573e09cbd46
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-06-26 21:39:06 -0300

    correcao na endentação do codigo

--- CRUD/CRUD_Pessoa.rb
+++ CRUD/CRUD_Pessoa.rb
@@ -1,8 +1,8 @@
 class Pessoa
-    attr_accessor :nome, :idade 
+    attr_accessor :nome, :idade
 
     def initialize(nome, idade)
-        @nome =  nome
+        @nome = nome
         @idade = idade
     end
 end
@@ -10,12 +10,13 @@ end
 class CrudPessoa
     def initialize
         @pessoas = []
+
     end
 
     def criar_pessoa(nome, idade)
         pessoa = Pessoa.new(nome, idade)
         @pessoas << pessoa
-        puts "Pessoa criada com sucesso!"
+        puts "Pessoa criadad com sucesso!"
     end
 
     def listar_pessoas
@@ -23,25 +24,25 @@ class CrudPessoa
             puts "Não há pessoas cadastradas. "
         else
             @pessoas.each do |pessoa|
-            puts "Nome: #{pessoa.nome}, Idade: #{pessoa.idade}"
+                puts "Nome: #{pessoa.nome}, Idade: #{pessoa.idade}"
+            end
         end
     end
-end
 
-def atualizar_pessoa(nome, nova_idade)
-    pessoa = @pessoas.find { |p| p.nome == nome }
-    if pessoa.nil?
-        puts "Pessoa não encontrada."
-    else
-        pessoa.idade = nova_idade
-        puts "Pessoa atualizada com sucesso!"
-    end
-end
-
-def excluir_pessoa(nome)
-    pessoa = @pessoas.find { |p| p.nome == nome }
+    def atualizar_pessoa(nome, nova_idade)
+        pessoa = @pessoas.find { |p| p.nome == nome }
         if pessoa.nil?
-            puts "Pessoa excluída com sucesso!"
+            puts "Pessoa não encontrada. "
+        else
+            @pessoa.idade = nova_idade
+            puts "Pessoa atualizada com sucesso!"
+        end
+    end
+
+    def excluir_pessoa(nome)
+        pessoa = @pessoas.find { |p| p.nome == nome }
+        if pessoa.nil?
+            puts "Pessoa excluída com sucesso! "
         end
     end
 end
@@ -51,13 +52,6 @@ crud = CrudPessoa.new
 
 crud.criar_pessoa("João", 25)
 crud.criar_pessoa("Maria", 30)
-
-crud.listar_pessoas
-#Saída:
-#Nome: João, idade: 25
-#Nome: Maria, idade: 30
-
-crud.atualizar_pessoa("João", 26)
 
 crud.listar_pessoas
 #Saída atualizada:
