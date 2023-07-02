7ff0a0ebd28d01ab1c07e83eaf1422c68d491ccc
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-01 23:53:20 -0300

    modificação no exemplo de projeto CRUD, realizando funçoes como: create, Read, update, delete

--- CRUD/CRUD_Pessoa2.rb
+++ CRUD/CRUD_Pessoa2.rb
@@ -30,7 +30,7 @@ class CrudPessoa
             end
         end
     end
-
+    
     def buscar_pessoa(id)
         pessoa = @pessoas.find { |p| p.id == id }
         if pessoa.nil?
@@ -39,47 +39,4 @@ class CrudPessoa
             puts "ID: #{pessoa.id}, Nome #{pessoa.nome}, Idade: #{pessoa.id}"
         end
     end
-
-    def atualizar_pessoa(id, nome, idade)
-        pessoa = @pessoas.find { |p| p.id == id }
-        if pessoa.nil?
-            puts "Pessoa não encontrada. "
-        else
-            pessoa.nome = nome
-            pessoa.idade = idade
-            puts "Pessoa atualizada com sucesso!"
-        end
-    end
-
-    def excluir_pessoa(id)
-        pessoa = @pessoas.find { |p| p.id == id }
-        if pessoa.nil?
-        puts "Pessoa não encontrada. "
-        else
-            @pessoas.delete(pessoa)
-            puts "Pessoa excluída com sucesso!"
-        end
-    end
-end
-
-# Exemplo de uso
-crud = CrudPessoa.new
-
-crud.criar_pessoa("João", 25)
-crud.criar_pessoa("Maria", 30)
-
-crud.listar_pessoas
-# Saída:
-#ID: 0, Nome: João, Idade: 25
-#ID: 1, Nome: Maria, Idade: 30
-
-crud.atualizar_pessoa(0, "João Silva", 26)
-
-crud.listar_pessoas
-#Saída atualizada:
-#ID: 0, Nome: João Silva, Idade: 26
-#ID: 1, Nome: Maria, Idade: 30
-
-crud.buscar_pessoa(1)
-#Saída:
-#ID: 1, Nome: Maria, Idade: 30
+    
