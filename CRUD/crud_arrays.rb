1724eb34b751a8fc5106a432e33116337945636e
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-03 22:42:57 -0300

    correção na saída do codigo esta apresentando 3 resultados

--- CRUD/crud_arrays.rb
+++ CRUD/crud_arrays.rb
@@ -26,11 +26,11 @@ class ProductManager
     end
 
     def find(id)
-        @products.find { |product| product.id == id }
+        @products.find { |product| product.id === id }
     end
 
     def update(id, name, price )
-        product - find(id)
+        product = find(id)
         return false unless product
 
         product.name = name
@@ -54,4 +54,14 @@ end
 #Exemplo de uso
 manager = ProductManager.new
 
-product1 = manager.create("Produto 1")
+product1 = manager.create("Produto 1", 10.99)
+puts product1
+
+product2 = manager.find(1)
+puts product2
+
+manager.update(1, "Novo Produto 1", 19.99)
+
+manager.all.each do |product|
+    puts product
+end
