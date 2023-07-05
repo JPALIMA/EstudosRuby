5ba6480003829f34f0979342f625863f34e70e5f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-04 22:38:57 -0300

    o método all foi modificado para imprimir cada produto individualmente em vez de retornar a lista de produtos

--- CRUD/crud_arrays.rb
+++ CRUD/crud_arrays.rb
@@ -1,60 +1,69 @@
 class Product
     attr_accessor :id, :name, :price
-
+  
     def initialize(id, name, price)
-        @id = id
-        @name = name
-        @price = price
+      @id = id
+      @name = name
+      @price = price
     end
-
+  
     def to_s
-        "ID: #{@id}, Name: #{@name}, Price: #{@price}"
+      "ID: #{@id}, Name: #{@name}, Price: #{@price}"
     end
-end
-
-class ProductManager
+  end
+  
+  class ProductManager
     def initialize
-        @products = []
-        @next_id = 1
+      @products = []
+      @next_id = 1
     end
-
+  
     def create(name, price)
-        product = Product.new(@next_id, name, price)
-        @products << product
-        @next_id += 1
-        product
+      product = Product.new(@next_id, name, price)
+      @products << product
+      @next_id += 1
+      product
     end
-
+  
     def find(id)
-        product = @products.find { |product| product.id === id }
-        product&.dup
+      @products.find { |product| product.id == id }
     end
-
+  
     def update(id, name, price)
-        product = find(id)
-        return false unless product
-
-        product.name = name
-        product.price = price
-        true
+      product = find(id)
+      return false unless product
+  
+      product.name = name
+      product.price = price
+      true
     end
-
+  
     def delete(id)
-        product = find(id)
-        return false unless product
-
-        @products.delete(product)
-        true
+      product = find(id)
+      return false unless product
+  
+      @products.delete(product)
+      true
     end
-
+  
     def all
-        @products.delete(product)
-        true
+      @products.each do |product|
+        puts products
+      end
     end
+  end
+  
+  # Exemplo de uso
+  manager = ProductManager.new
+  
+  product1 = manager.create("Produto 1", 10.99)
+  puts product1
+  
+  product2 = manager.find(1)
+  puts product2
+  
+  manager.update(1, "Novo Produto 1", 19.99)
+  
+  manager.all
 
-    def all
-        @products
-    end
-end
-
-#Exmplo de uso
+  end
