a2197aa9f32f6a9c2c9f70fd4ee78b290f0bf9aa
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-14 20:50:04 -0300

    implementação simples de um CRUD em Ruby, usando JSON

---
+++ CRUD/crud_usando_json.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class User
+    attr_accessor :id, :name, :email
+
+    def initialize(id, name, email)
+        @id = id
+        @name = name
+        @email = email
+    end
+end
+
+class UserRepository
+    def initialize(file_path)
+        @file_path = file_path
+        @users = load_users || []
+        @next_id = calculate_next_id
+    end
+    
+    def create(name, email)
+        user = User.new(@next_id, name, email)
+        @users << user
+        @next_id += 1
+        save_users
+        user
+    end
+
+    def find(id)
+        @users.find { |user| user.id == id }
