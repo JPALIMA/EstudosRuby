d7d5b34a98183c2b50b525ebe5a42a4e8f1b3ad1
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-17 21:56:53 -0300

    criar um catalogo de produtos

--- product_manager.rb
+++ product_manager.rb
@@ -34,3 +34,36 @@ class ProductManager
   end
   
   def list_products
+    @products
+  end
+
+  def find_product(product_id)
+    @products.find { |product| product.id == product_id }
+  end
+
+  def update_product(product_id, name, decription, price, quantity)
+    product = find_product(product_id)
+    return unless product
+
+    product.name = name
+    product.decription = description
+    product.price = price
+    product.quantity = quantity
+    product
+  end
+
+  def delete_product(product_id)
+    product = find_product(product_id)
+    return unless product
+
+    @products.delete(product)
+    product
+  end
+end
+
+#main.rb
+require_relative 'product_manager'
+
+product_manager = ProductManager.new
+
+#Adcionar produtos
