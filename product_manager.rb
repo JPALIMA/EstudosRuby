aaebfec2fcd9768745e0c9ec257d37e036676e2d
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-19 22:02:35 -0300

    projeto de catálogo de produtos

--- product_manager.rb
+++ product_manager.rb
@@ -43,8 +43,39 @@ class ProductManager
 
     product.name = name
     product.description = description
+    product.price = price
     product.quantity = quantity
     product
   end
 
-  
+  def delete_product(product_id)
+    product = find_product(product_id)
+    return unless product
+
+    @products.delete(product)
+    product
+  end
+end
+
+#main.rb
+require_relative 'product_manager'
+
+product_manager = ProductManager.new
+
+#Adicionar produtos
+product_manager.add_product('produto 1', 'Descrição do Produto 1', 10.99, 100)
+product_manager.add_product('produto 2', 'Descrição do Produto 2', 19.99, 50)
+
+#Listar produtos
+products = product_manager.list_products
+products.each do |product|
+  puts "ID: #{product.id}, Nome: #{product.name}, Descrição: #{product.description}, Preço: #{product.price},
+  quantidade: #{product.quatity}"
+end
+
+#Atualizar produto
+product_id = products.first.id
+product_manager.update_product(product_id, 'Novo Nome', 'Nova Descrição', 15.99, 75)
+
+#Excluir produto
+deleted_product = product_manager.delete_product(product_id)
