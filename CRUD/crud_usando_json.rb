649876969c030704eb9e89a641076c9060f0aba6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-20 21:01:28 -0300

    aplicação de um projeto em ruby usando json

--- CRUD/crud_usando_json.rb
+++ CRUD/crud_usando_json.rb
@@ -33,3 +33,59 @@ class UserRepository
         user = find(id)
         return nil unless user
         
+        user.name = name
+        user.email = email
+        save_users
+        user
+    end
+
+    def delete(id)
+        user = find(id)
+        return nil unless user
+
+        @users.delete(user)
+        save_users
+        user
+    end
+
+    def all
+        @users
+    end
+
+    private
+
+    def load_users
+        return nil unless File.exist?(@file_path)
+
+        data = File.read(@file_path)
+        user_data = JSON.parse(data)
+        user_data.map { |user| User.new(user['id'], user['name'], user['email'])}
+    end
+
+    def calculate_next_id
+        return 1 if @users.empty?
+
+        max_id = @users.map(&:id).compact.max
+        max_id ? max_id + 1 : 1
+    end
+
+    def save_users
+        data = @users.map { |user| {id: user.id, name: user.name, email: user.email }}
+        File.write(@file_path, JSON.generate(data))
+    end
+end
+
+#Exemplo de uso
+user_repository = UserRepository.new('users.json')
+
+user1 = user_repository.create('Joaozinho', 'joaozinho@exemplo.com')
+user2 = user_repository.create('Joao', 'joao@exemplo.com')
+
+puts user1.name #Saída: joaozinho
+puts user2.email #Saída: joao@exemplo.com
+
+user_repository.update(user1.id, 'joao', 'joao@exemplo.com')
+puts user1.name #saída: joao
+
+user_repository.delete(user2.id)
+puts user_repository.all.length #saída: 1
