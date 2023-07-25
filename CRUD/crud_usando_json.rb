c426fc50df87c90b893d224dc0ecec623dbddba6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-25 20:22:24 -0300

    aplicação de codigo em ruby usando json

--- CRUD/crud_usando_json.rb
+++ CRUD/crud_usando_json.rb
@@ -1,4 +1,4 @@
-require 'json'
+require 'Json'
 
 class User
     attr_accessor :id, :name, :email
@@ -32,7 +32,7 @@ class UserRepository
     def update(id, name, email)
         user = find(id)
         return nil unless user
-
+        
         user.name = name
         user.email = email
         save_users
@@ -68,21 +68,21 @@ class UserRepository
         max_id = @users.map(&:id).compact.max
         max_id ? max_id + 1 : 1
     end
-
+    
     def save_users
         data = @users.map { |user| {id: user.id, name: user.name, email: user.email}}
         File.write(@file_path, JSON.generate(data))
     end
 end
 
-#Exemplo de uso
+#exemplo de uso
 user_repository = UserRepository.new('users.json')
 
-user1 = user_repository.create('Joaozinho', 'joaozinho@exemplo.com')
+user1 = user_repository.create('joaozinho', 'joaozinho@exemplo.com')
 user2 = user_repository.create('joao', 'joao@exemplo.com')
 
-puts user1.name #Saída: joaozinho
-puts user2.email #SAÍDA: joao@exemplo.com
+puts user1.name #Save: joaozinho
+puts user2.email #Saída: joao@exemplo.com
 
 user_repository.update(user1.id, 'joao', 'joao@exemplo.com')
 puts user1.name #saída: joao
