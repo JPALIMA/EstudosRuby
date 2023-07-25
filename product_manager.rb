c952b2f3339c4913ba2d2f131b6cc773d487e07f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-25 20:03:21 -0300

    correção de sintaxe nas linhas  23, a classe estava 'product_Manager', linha 51 o produto 'product.quantity' estava 'pruduct.quantity e outros

--- product_manager.rb
+++ product_manager.rb
@@ -20,7 +20,7 @@ end
 #product_manager.rb
 require 'securerandom'
 
-class product_manager
+class ProductManager
   attr_accessor :products
 
   def initialize
@@ -38,9 +38,48 @@ class product_manager
   end
 
   def find_product(product_id)
-    @products_find { |product| product.id == product_id }
+    @products.find { |product| product.id == product_id }
   end
 
   def update_product(product_id, name, description, price, quantity)
     product = find_product(product_id)
     return unless product
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+    product
+  end
+
+  def delete_product(product_id)
+    product = find_product(product_id)
+    return unless product
+
+    @products.delete(product)
+    product
+  end
+end
+
+#main.rb
+require_relative 'product_manager'
+
+product_manager = ProductManager.new
+
+#adicionar produtos
+product_manager.add_product('produto 1', 'Descrição do Produto 1', 10.00, 100)
+product_manager.add_product('produto 2', 'Descrição do Produto 2', 19.99, 50)
+
+#Listar produtos
+products = product_manager.list_products
+products.each do |product|
+   puts "ID: #{product.id}, Nome: #{product.name}, Descrição: #{product.description}, Preço: #{product.price}
+   quantidade: #{product.quantity}"
+end
+
+#atualizar produto
+product_id = products.first.id
+product_manager.update_product(product_id, 'Novo name', 'Novo Descrição', 15.99, 75)
+
+#Excluir produto
+deleted_product = product_manager.delete_product(product_id)
