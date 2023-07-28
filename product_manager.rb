d8c471276d9ffe6f152cab634686b81389bbe394
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-07-27 21:12:49 -0300

    criação de codigo em ruby implementando catalogo de preço

--- product_manager.rb
+++ product_manager.rb
@@ -7,7 +7,7 @@ class Product
     @name = name
     @description = description
     @price = price
-    @quantity = quantity
+    @quanity = quantity
   end
 
   private
@@ -28,58 +28,4 @@ class ProductManager
   end
 
   def add_product(name, description, price, quantity)
-    product = Product.new(name, description, price, quantity)
-    @products << product
-    product
-  end
-
-  def list_products
-    @products
-  end
-
-  def find_product(product_id)
-    @products.find { |product| product.id == product_id }
-  end
-
-  def update_product(product_id, name, description, price, quantity)
-    product = find_product(product_id)
-    return unless product
-
-    product.name = name
-    product.description = description
-    product.price = price
-    product.quantity = quantity
-    product
-  end
-
-  def delete_product(product_id)
-    product = find_product(product_id)
-    return unless product
-
-    @products.delete(product)
-    product
-  end
-end
-
-#main.rb
-require_relative 'product_manager'
-
-product_manager = ProductManager.new
-
-#adicionar produtos
-product_manager.add_product('produto 1', 'Descrição do Produto 1', 10.00, 100)
-product_manager.add_product('produto 2', 'Descrição do Produto 2', 19.99, 50)
-
-#Listar produtos
-products = product_manager.list_products
-products.each do |product|
-  puts "ID: #{product.id}, Nome: #{product.name}, Descrição: #{product.description}, Preço: #{product.price}
-  quantidade: #{product.quantity}"
-end
-
-#atualizar produto
-product_id = products.first.id
-product_manager.update_product(product_id, 'Novo name', 'Novo Descrição', 15.99, 75)
-
-#Excluir produto
-delete_product = product_manager.delete_product(product_id)
+    product = Product.new(name)
